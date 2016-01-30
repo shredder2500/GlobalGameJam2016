ee7884c865c315823fbe97b9d215f8ac84733018
Christopher Taylor <christopher.taylor@rtegaming.com>
2016-01-30 17:17:43 -0600

    Health bar

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GGJ.UI
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        private float _scale;
+
+        public float Value
+        {
+            get
+            {
+                return transform.localScale.x;
+            }
+            set
+            {
+                var scale = transform.localScale;
+                scale.x = _scale * value;
+                if(scale.x < 0)
+                {
+                    scale.x = 0;
+                }
+                transform.localScale = scale;
+            }
+        }
+
+        private void Start()
+        {
+            _scale = transform.localScale.x;
+        }
+    }
+}
