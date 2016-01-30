dd52e7b80051ae2542bce202b1a1e00a4fd506a8
sunyudai <Josh.Patton.D@gmail.com>
2016-01-30 16:43:22 -0600

    Adding victory condition to ritual

--- Assets/Scripts/State/RitualSite.cs
+++ Assets/Scripts/State/RitualSite.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class RitualSite : MonoBehaviour 
 {
+    [SerializeField]
+    private string _nextScene;
+
     [SerializeField]
     private bool _isLightSide;
 
@@ -25,6 +29,21 @@ public class RitualSite : MonoBehaviour
             foreach (var ritualItem in keys.Where(key => Inventory.Items[key] == InventoryItem.LocationEnum.InInventory))
             {
                 Inventory.Items[ritualItem] = InventoryItem.LocationEnum.AtRitualSite;
+            }
+
+
+            if(_isLightSide && Ritual.HasLightSideItems)
+            {
+                //TODO: Open ritual portal.
+            }
+            else if (!_isLightSide && Ritual.HasDarkSideItems)
+            {
+                //TODO: Open ritual portal.
+            }
+
+            if (Ritual.HasAllItems)
+            {
+                SceneManager.LoadScene(_nextScene);
             }
         }
     }
