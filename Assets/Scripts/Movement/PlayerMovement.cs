5a96d7638f8ad19567a2ee36cac58c33d2d17add
Christopher Taylor <christopher.taylor@rtegaming.com>
2016-02-06 11:19:09 -0600

    Player Position sync between scenes

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -5,6 +5,8 @@ namespace GGJ.Movement
 {
     public class PlayerMovement : BaseMovement
     {
+        private static Vector3 _position;
+
         private GameTimer _timer = null;
         [SerializeField]
         private float _damageTimeLose;
@@ -17,12 +19,18 @@ namespace GGJ.Movement
 
         protected override void OnStart()
         {
+            transform.position = _position;
             _timer = FindObjectOfType<GameTimer>();
 
             _onDamage = () => _timer.LoseTime(_damageTimeLose);
             _onDoDamage = () => _timer.GainTime(_doDamageTimeGain);
 
             Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Enemy"));
+        }
+
+        private void OnDestroy()
+        {
+            _position = transform.position;
         }
     }
 }
