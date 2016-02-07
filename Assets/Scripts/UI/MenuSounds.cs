8f51507e0656f3c689d895a8c0cb1ce9701a6e8e
Ageonix <smoulton98@hotmail.com>
2016-02-07 17:00:07 -0600

    Forgot some sounds.

---
+++ Assets/Scripts/UI/MenuSounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSounds : MonoBehaviour
+{
+    private AudioSource _menuAudioSource;
+
+    [SerializeField]
+    private AudioClip _menuClickSound;
+    
+    void Start()
+    {
+        _menuAudioSource = this.gameObject.AddComponent<AudioSource>();
+        _menuAudioSource.clip = _menuClickSound;
+        DontDestroyOnLoad(_menuAudioSource);
+    }
+
+    public void PlayClickSound()
+    {
+        _menuAudioSource.Play();
+    }
+}
